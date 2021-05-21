b99ead99e9830264691ce940017e6700caa146d8
Petter Nilsson <pettergnilsson@gmail.com>
2021-05-21 11:02:48 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Petter Nilsson on 2021-05-21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
